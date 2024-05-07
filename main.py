578b4f731e8906df1fe37e00a1d311541151d95d
sinyalat95 <ashganmusa@windowslive.com>
2024-05-07 15:16:18 +0200

    3rd assessment

--- main.py
+++ main.py
@@ -2,13 +2,27 @@
 Upper and Lower
 """
 # Provide your solution here
+def count_upper_lower(word) -> None:
+    for i in word:
+        i.isupper()
 
+    print(f"No. of Upper case characters: ", {count_upper_lower(word)})
+    print(f"No. of Lower case characters: ", {count_upper_lower(word)})
 """
 Check 33
 """
 # Provide your solution here
 
+def has_33(my_list) -> bool:
+    for i in my_list:
+        if my_list[i] == 3:
+            return bool
+        print(has_33)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("Test your functions by calling them here. Use different parameter values to test them with different scenarios.")
-
+    print("Test your functions by calling them here. Use different parameter values to test them with different "
+          "scenarios.")
+my_list = [1, 5, 5, 3, 5, 4]
+print(has_33(my_list))
