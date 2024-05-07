38b08e26d11d4aeea163252ce9df9ca45b31007c
sinyalat95 <162726071+sinyalat95@users.noreply.github.com>
2024-05-07 14:04:36 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Upper and Lower
+"""
+# Provide your solution here
+
+"""
+Check 33
+"""
+# Provide your solution here
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Test your functions by calling them here. Use different parameter values to test them with different scenarios.")
+
